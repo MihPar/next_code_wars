65e6bed9bcc700891878d4023753e996c14740d2
MihPar <9496260@gmail.com>
2023-05-26 16:51:48 +0300

    two

--- script04.js
+++ script04.js
@@ -14,4 +14,4 @@ function outed(meet, boss){
 		return "Nice Work Champ!"
 	}
 }
-console.log(outed({'tim':0, 'jim':2, 'randy':0, 'sandy':7, 'andy':0, 'katie':5, 'laura':1, 'saajid':2, 'alex':3, 'john':2, 'mr':0}, 'laura'))
+console.log(outed({'tim':0, 'jim':2, 'randy':0, 'sandy':7, 'andy':0, 'katie':5, 'laura':1, 'saajid':2, 'alex':3, 'john':2, 'mr':0}, 'laura'))           
