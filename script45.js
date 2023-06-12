c543ca76fb322ec0cd5bfd6123e9ff0a5d3a4e9f
MihPar <9496260@gmail.com>
2023-06-12 16:58:33 +0300

    nnn

---
+++ script45.js
@@ -0,0 +1,8 @@
+var min = function(list){
+    return Math.min(...list)
+}
+console.log(min([-52, 56, 30, 29, -54, 0, -110]))
+var max = function(list){
+    return Math.max(...list)    
+}
+console.log(max([-52, 56, 30, 29, -54, 0, -110]))
