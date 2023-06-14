bee04534d0759211030153ad1157708713befac8
MihPar <9496260@gmail.com>
2023-06-14 11:20:44 +0300

    num

---
+++ script50.js
@@ -0,0 +1,16 @@
+function SubtractSum(n){
+    return "apple"
+  }
+
+//   function SubtractSum(n){
+//     let arr = n.toString().split("")
+//     console.log(arr)
+   
+//     let num = 0
+//     for(let i = 0; i < arr.length; i++) {
+//      num += +arr[i]
+//     }
+//     return n -= num
+//    }
+//    console.log(SubtractSum(325))
+   
