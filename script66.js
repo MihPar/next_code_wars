a64d9dc0ce8210d26b19a8c903d548ac9e9c6e87
MihPar <9496260@gmail.com>
2023-06-18 15:27:26 +0300

    ksksk

---
+++ script66.js
@@ -0,0 +1,6 @@
+function position(letter){
+    return `Position of alphabet: ${alphabet.indexOf(letter.toUpperCase()) + 1}`
+  }
+  let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  position('a')
+  
