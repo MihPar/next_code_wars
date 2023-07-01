053551128673b576d9a761a410dea61aba118a4b
MihPar <9496260@gmail.com>
2023-07-01 14:08:40 +0300

    ncdhery

--- script110.js
+++ script110.js
@@ -3,7 +3,9 @@ function isPythagoreanTriple(integers) {
     for(let i = 0; i < integers.length; i++) {
       arr.push(integers[i])
     }
-    let n = arr.sort()
+    let n = arr.sort(function(a, b) {
+      return a - b
+    })
     return (n[0] ** 2 ) + (n[1] ** 2) === (n[2] ** 2) ? true : false
   }
   console.log(isPythagoreanTriple([72, 78, 30]))
