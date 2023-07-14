dab1c9aa07566370404a9989b6a5ddd95efa5997
MihPar <9496260@gmail.com>
2023-07-14 15:17:32 +0300

    askjfasfjs

---
+++ script136.js
@@ -0,0 +1,31 @@
+
+function greet(language) {
+  
+    const object = {
+          english: 'Welcome',
+          czech: 'Vitejte',
+          danish: 'Velkomst',
+          dutch: 'Welkom',
+          estonian: 'Tere tulemast',
+          finnish: 'Tervetuloa',
+          flemish: 'Welgekomen',
+          french: 'Bienvenue',
+          german: 'Willkommen',
+          irish: 'Failte',
+          italian: 'Benvenuto',
+          latvian: 'Gaidits',
+          lithuanian: 'Laukiamas',
+          polish: 'Witamy',
+          spanish: 'Bienvenido',
+          swedish: 'Valkommen',
+          welsh: 'Croeso'
+  }
+    
+    for(let key in object) {
+      if(key === language) {
+        return object[key]
+      } 
+    }
+return object['english']
+}
+
