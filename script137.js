994a707f01759b30784cf9b378caa0c885ea656f
MihPar <9496260@gmail.com>
2023-07-15 15:52:35 +0300

    alkfjskfjdsafkj

---
+++ script137.js
@@ -0,0 +1,8 @@
+function generateRange(min, max, step){
+    let arr = []
+    for(let i = min; i <= max; i += step) {
+      arr.push(i)
+    }
+    return arr
+  }
+  console.log(generateRange(2, 10, 2))
