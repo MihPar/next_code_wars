6b5b286ae12407f04370dbbebb667c196b4452c4
MihPar <9496260@gmail.com>
2023-08-06 14:40:11 +0300

    ksdfjsdfj

---
+++ script182.js
@@ -0,0 +1,18 @@
+// Add rooms here
+var rooms = {
+    name: {
+      one: "1",
+      tow: "2",
+      three: "3"
+    },
+    description: {
+      one: '4',
+      two: '5',
+      three: '6'
+    },
+    completed: {
+      one: "7",
+      two: "8",
+      three: "9"
+    }
+  }
