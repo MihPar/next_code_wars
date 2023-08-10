4b75a1783534003dcf8dca1e3940dfb656bd2b79
MihPar <9496260@gmail.com>
2023-08-10 11:51:08 +0300

    sdjksdjfdsjfjoi

---
+++ script191.js
@@ -0,0 +1,8 @@
+// bool set_alarm( const bool& employed, const bool& vacation )
+// {
+//   return (employed && !vacation);
+// }
+
+function setAlarm(employed, vacation){
+    return employed && !vacation;
+  }
