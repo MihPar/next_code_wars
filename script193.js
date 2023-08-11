b52e3677d3cec6d80e40b79bc0320e0998684b83
MihPar <9496260@gmail.com>
2023-08-11 14:18:03 +0300

    ksdfjkjfweiu

---
+++ script193.js
@@ -0,0 +1,5 @@
+function sayHello( name, city, state ) {
+    return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
+  }
+  // console.log(sayHello(['John', 'Smith'], 'Phoenix', 'Arizona'))
+  console.log(sayHello(['Franklin','Delano','Roosevelt'], 'Chicago', 'Illinois'))
