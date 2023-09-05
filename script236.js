13263fd8a001a788b7a5f639c6a744d161f7aa25
MihPar <9496260@gmail.com>
2023-09-05 07:45:20 +0300

    c_w237

--- script236.js
+++ script236.js
@@ -4,3 +4,32 @@ function twoHighest(arr) {
 	   .sort((a, b) => b - a)  // Sort by DESC
 	   .slice(0, 2);  // Get first 2 element of sorted array
    }
+
+//    function twoHighest(arr) {
+// 	if (arr.length === 0) {
+// 		return []
+// 	} else {
+// 		for(let i = 0; i < arr.length; i++) {
+// 			if(arr[i] === arr[i + 1]) {
+// 				return [arr[i]]
+// 			}
+// 		}
+// 	}
+//   let num = 0
+//   let min = 0
+	
+//   for(let i = 0; i < arr.length; i++) {
+//     if(arr[i] > num) {
+//       num = arr[i]
+// 	}
+//   }
+//   for(let i = 0; i < arr.length; i++) {
+// 		if(num > arr[i]) {
+// 			min = arr[i]
+// 		}
+// 	}
+// 	return [num, min]
+
+
+// }
+// console.log(twoHighest([4, 10, 10, 9] ))
