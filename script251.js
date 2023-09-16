9b9360597fb05f20f05247641c25207ff8f94b64
MihPar <9496260@gmail.com>
2023-09-16 11:26:51 +0300

    c_w251/1

--- script251.js
+++ script251.js
@@ -1,3 +1,5 @@
 function sumOfIntegersInString(s) {
-	return (s.match(/\d+/g) || []).reduce((s, n) => s + +n, 0);
+	// return (s.match(/\d+/g) || []).reduce((s, n) => s + +n, 0);
+
+	return s.split(/[^0-9]/g).map(item => parseInt(item)).filter(Boolean).reduce((accum, item) => accum + item, 0)
   }
